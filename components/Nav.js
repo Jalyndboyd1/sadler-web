3ac110a4bc31f7daa733e2265861ed0efe9b5411
Jalyn.Boyd <Jalyn.Boyd@oneamerica.com>
2022-10-23 22:25:17 -0400

    CSS / Image Changes to Nav

--- components/Nav.js
+++ components/Nav.js
@@ -4,7 +4,7 @@ import NavLink from './utills/NavLink'
 
 function Nav() {
     return (
-        <div className='flex justify-between p-2.5 items-center bg-sadler-purple'>
+        <div className='flex justify-between p-2.5 pl-5 items-center bg-black'>
             {/* Nav Left / Img */}
             <Image src={logo} alt=""
                 width={85}
