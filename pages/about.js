cdf1bec885224406e36e44a9c57956ffe979f26a
Jalyn.Boyd <Jalyn.Boyd@oneamerica.com>
2022-10-26 00:54:07 -0400

    Wave added to about page

--- pages/about.js
+++ pages/about.js
@@ -3,16 +3,21 @@ import aboutImg from '../public/about.jpg'
 
 function About() {
     return (
-        <div className='flex items-center justify-between px-28 py-20'>
-            {/* image left */}
-            <Image src={aboutImg} alt='' height='380px' width='380px' objectFit='contain'/>
-            {/* description right */}
-            <div>
-                <h1 className='font-light text-3xl ml-20'>
-                    Sadler is building a focus on protecting
-                    our abuse clients with modern technology in the world of
-                    social services!
-                </h1>
+        <div>
+            <div className='flex items-center justify-between px-28 py-20'>
+                {/* image left */}
+                <Image src={aboutImg} alt='' height='380px' width='380px' objectFit='contain' />
+                {/* description right */}
+                <div>
+                    <h1 className='font-light text-3xl ml-20'>
+                        Sadler is building a focus on protecting
+                        our abuse clients with modern technology in the world of
+                        social services!
+                    </h1>
+                </div>
+            </div>
+            <div className="h-96 bg-[url('../public/wave.png')] bg-cover bg-center">
+                {/* words */}
             </div>
         </div>
     )
